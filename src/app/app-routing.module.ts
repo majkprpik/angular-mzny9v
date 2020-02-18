149457d9816120c5bef1513d1075986abd456528
majkprpik <vedran.prpic1@gmail.com>
2020-02-18 10:27:44 +0100

    5 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,30 @@
-import { Router, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
-import { ProfileComponent } from './profile/profile.component';
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AboutComponent } from "./about/about.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { NgModule } from "@angular/core";
 
 const routes: Routes = [
   {
-    path: 'about', 
+    path: "about",
     component: AboutComponent
   },
   {
-    path: 'profile',
-    component: ProfileComponent,
+    path: "profile",
+    component: ProfileComponent
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "**",
     component: DashboardComponent
   }
-]
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { AboutModule } from './about/about.module';
 import { ProfileModule } from './profile/profile.module';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AboutComponent } from "./about/about.component";
+import { ProfileComponent } from "./profile/profile.component";
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, DashboardModule, AboutModule, ProfileModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, ,AppRoutingModule ],
+  declarations: [ AppComponent, DashboardComponent, AboutComponent, ProfileComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
